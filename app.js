93994739911a62375ed7936c4f166300bf0ba5e6
cmrotruck <crotruck@gmail.com>
2021-12-21 17:36:19 -0500

    added ability to enter multiple projects

--- app.js
+++ app.js
@@ -1,15 +1,105 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const inquirer = require('inquirer');
+const { stubFalse } = require('lodash');
 
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i++){
-        console.log(profileDataArr[i]);
-    }
-
-    console.log("==========");
-
-    // Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));    
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name? '
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your GitHub Username: '
+        },
+        {
+            type: 'input',
+            name: 'about',
+            message: 'Provide some information about yourself: '
+        }
+    ]);
 };
 
-printProfileData(profileDataArgs);
+const promptProject = portfolioData => {
+    // If there's no 'projects' array property, create one
+    if (!portfolioData.projects) {
+        portfolioData.projects = [];
+    }
+    console.log(`
+========================
+Add a new Project 
+========================
+`);
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of your project?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a description of the project (required)'
+        },
+        {
+            type: 'checkbox',
+            name: 'languages',
+            message: 'What did you build this project with? (Check all that apply)',
+            choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'Jquery', 'Bootstrap', 'Node']
+        },
+        {
+            type: 'input',
+            name: 'link',
+            message: 'Enter the GitHub link to your project. (Required)'
+        },
+        {
+            type: 'confirm',
+            name: 'feature',
+            message: 'Would you like to feature this project?',
+            default: false
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAddProject',
+            message: 'Would you like to enter another project?',
+            default: false
+        }
+    ])
+    .then(projectData => {
+        portfolioData.projects.push(projectData);
+        if (projectData.confirmAddProject) {
+          return promptProject(portfolioData);
+        } else {
+          return portfolioData;
+        }
+      });
+};
+
+promptUser()
+  .then(promptProject)
+  .then(portfolioData => {
+    console.log(portfolioData);
+});
+
+// promptUser()
+//     .then(answers => console.log(answers))
+//     .then(promptProject)
+//     .then(projectAnswers => console.log(projectAnswers))
+//     .then(projectData => {
+//         if (projectData.confirmAddProject) {
+//             return promptProject(portfolioData);
+//         } else {
+//             return portfolioData;
+//         }
+//     });
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
+
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
